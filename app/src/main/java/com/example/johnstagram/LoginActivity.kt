5ce9f5ca4625ad56a60939cd401f9d1908cd4240
joh9911 <joh9911@naver.com>
2022-09-22 14:41:12 +0900

    Johnstagram

--- app/src/main/java/com/example/johnstagram/LoginActivity.kt
+++ app/src/main/java/com/example/johnstagram/LoginActivity.kt
@@ -3,9 +3,12 @@ package com.example.johnstagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_page)
+    }
+    fun midLinearLayoutMove(){
+
     }
 }
