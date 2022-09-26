4a3437c519471e9997c00d3c8b7d023eadb8c471
joh9911 <joh9911@naver.com>
2022-09-26 23:59:40 +0900

    Johnstagram

--- app/src/main/java/com/example/johnstagram/SignUpFragment.kt
+++ app/src/main/java/com/example/johnstagram/SignUpFragment.kt
@@ -1,14 +1,17 @@
 package com.example.johnstagram
 
+import android.Manifest
 import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.content.Context.TELEPHONY_SERVICE
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.telephony.TelephonyManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.johnstagram.databinding.SignupFragmentBinding
 import com.google.android.material.tabs.TabLayout
@@ -29,9 +32,9 @@ class SignUpFragment: Fragment() {
         return view
     }
     fun initEvent(){
-        binding.signUpEmailEdittext.visibility = View.GONE
         val imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         binding.signUpPhoneNumberEdittext.requestFocus()
+        binding.signUpPhoneNumberEdittext.setText(getPhoneNum(requireContext()))
         if(binding.signUpPhoneNumberEdittext.isFocused){
             imm.showSoftInput(binding.signUpPhoneNumberEdittext, InputMethodManager.SHOW_FORCED)
         }
@@ -61,7 +64,9 @@ class SignUpFragment: Fragment() {
             }
         })
         returnToLoginFragmentEvent()
+        phoneNumEditTextEraseButtonEvent()
     }
+
     fun returnToLoginFragmentEvent(){
         binding.signUpPageBottomLinearLayout.setOnClickListener {
             val dataInterface = context as ReplaceFragmentToLoginFragment
@@ -70,7 +75,48 @@ class SignUpFragment: Fragment() {
             binding.signUpPhoneNumberEdittext.text.clear()
         }
     }
-    fun phoneNumEditTextEraseButtonEvent(){
 
+    fun getPhoneNum(context: Context): String? {
+        var phoneNum = ""
+        val telManager = context.getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+//        if (ActivityCompat.checkSelfPermission(
+//                context,
+//                Manifest.permission.READ_SMS
+//            ) != PackageManager.PERMISSION_GRANTED )
+//            return "0"
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.INTERNET
+            ) != PackageManager.PERMISSION_GRANTED )
+            return "123"
+        else if(ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED)
+            return "01"
+        else if(
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.READ_PHONE_STATE
+            ) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "010"
+        }
+        phoneNum = telManager.line1Number.toString()
+        if (phoneNum.startsWith("+82")) {
+            phoneNum = phoneNum.replace("+82", "0")
+        }
+        return phoneNum
+    }
+
+    fun phoneNumEditTextEraseButtonEvent(){
+        binding.signUpPageClearButton.setOnClickListener {
+            binding.signUpPhoneNumberEdittext.text.clear()
+        }
     }
 }
