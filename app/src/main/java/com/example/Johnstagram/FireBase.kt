49231e1972211930235202bee44447d2e9ac3df5
joh9911 <joh9911@naver.com>
2022-10-05 23:34:38 +0900

    Johnstgram

---
+++ app/src/main/java/com/example/Johnstagram/FireBase.kt
@@ -0,0 +1,11 @@
+package com.example.Johnstagram
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+object FireBase {
+    val auth = Firebase.auth
+    var verificationId = ""
+
+
+}
